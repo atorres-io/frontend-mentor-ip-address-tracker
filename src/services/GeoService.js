c56a24de1c2f99dbbbd3d2225f549b77e2e3baf1
Alejandro Torres <atorresalcala.me@gmail.com>
2022-04-06 08:51:11 +0200

    perf(v2): upgrade version packages

--- src/services/GeoService.js
+++ src/services/GeoService.js
@@ -8,7 +8,7 @@ export const getGeoDatas = async () => {
 	});
 
 	const geo = await call(
-		`${CORS}https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${ip}`,
+		`${CORS}https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip}`,
 		{ method: 'GET', type: 'json' }
 	);
 
--- src/services/SchemeService.js
+++ src/services/SchemeService.js
@@ -1,8 +1,6 @@
-export const DARK_MAP =
-	'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png';
+export const DARK_MAP = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-export const LIGHT_MAP =
-	'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png';
+export const LIGHT_MAP = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
 export const preferDarkScheme =
 	window.matchMedia &&
